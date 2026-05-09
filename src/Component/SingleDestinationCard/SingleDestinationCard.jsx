1c2fbf282a47d9b45e7b040af0c0dd457f92faa6
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-05-10 03:20:41 +0600

    card to book now btn click transfer user from details and book page done

---
+++ src/Component/SingleDestinationCard/SingleDestinationCard.jsx
@@ -0,0 +1,54 @@
+import { Button, Card } from '@heroui/react';
+import Image from 'next/image';
+import React from 'react';
+import { FaArrowRightLong } from 'react-icons/fa6';
+import { SlLocationPin } from 'react-icons/sl';
+import { MdEditLocationAlt } from "react-icons/md";
+import Link from 'next/link';
+import { ArrowShapeTurnUpLeft, ArrowUpLeft, ArrowUpRightFromSquare } from '@gravity-ui/icons';
+
+const SingleDestinationCard = ({ data }) => {
+    const { destinationName, country, price, duration, departureDate, imageUrl, description } = data
+    return (
+        <div className='my-10'>
+            <div className='flex justify-between items-center p-1 my-4'>
+                <Link href={'/destination'} className='flex items-center gap-1 underline text-neutral-600'>
+                    <ArrowUpLeft></ArrowUpLeft>
+                    Back to Destinations
+                </Link>
+                <div>
+                    <Button className={'rounded-md border-gray-300'} variant='outline'><MdEditLocationAlt /> Edit</Button>
+                </div>
+            </div>
+            <Card variant="" className={'rounded-md'}>
+                <Image src={imageUrl} className='w-full rounded-md h-[600px]' height={200} width={200} alt={destinationName}></Image>
+                <span className='flex items-center gap-1 text-neutral-500 mt-3'><SlLocationPin /> {country}</span>
+                <Card.Header className='grid grid-cols-12 gap-4  justify-between'>
+                    <div className=' space-y-2 col-span-10 mt-4'>
+                        <Card.Title className='text-2xl font-semibold'>{destinationName}</Card.Title>
+                        <Card.Description>{duration}</Card.Description>
+
+                        <h2 className='text-2xl font-semibold'>Overview</h2>
+                        <p className='text-neutral-500'>{description}</p>
+                    </div>
+                    <Card className=' rounded-md  col-span-2'>
+                        <div>
+                            <p className='text-neutral-500'>Starting from</p>
+                            <h3 className='text-xl text-cyan-500'>${price}</h3>
+                            <p className='text-neutral-500'>per person</p>
+                        </div>
+                        <span className='border border-gray-300 text-center p-0.5 bg-gray-300 rounded-md mt-3'>{departureDate}</span>
+                        <Button className={' rounded-md bg-cyan-500 text-white w-full'} variant='outline'>Book Now <FaArrowRightLong /></Button>
+                        <ul className=' list-disc text-[13px] text-neutral-500'>
+                            <li>Free cancellation up to 7 days</li>
+                            <li>Travel insurance included</li>
+                            <li>24/7 customer support</li>
+                        </ul>
+                    </Card>
+                </Card.Header>
+            </Card>
+        </div>
+    );
+};
+
+export default SingleDestinationCard;
--- src/app/destination/[id]/page.jsx
+++ src/app/destination/[id]/page.jsx
@@ -1,9 +1,15 @@
+import SingleDestinationCard from '@/Component/SingleDestinationCard/SingleDestinationCard';
 import React from 'react';
 
-const DetailsPage = () => {
+const DetailsPage = async ({ params }) => {
+    const { id } = await params
+    const res = await fetch(`http://localhost:5001/destination/${id}`)
+    const data = await res.json()
+
+
     return (
-        <div>
-            
+        <div className=' container mx-auto'>
+            <SingleDestinationCard data={data}></SingleDestinationCard>
         </div>
     );
 };
