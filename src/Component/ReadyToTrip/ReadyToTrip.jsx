430a8afa8c85df47236dd07e96610bbfb1ab0667
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-05-14 05:01:51 +0600

    ready to trip section is done

--- src/Component/ReadyToTrip/ReadyToTrip.jsx
+++ src/Component/ReadyToTrip/ReadyToTrip.jsx
@@ -5,12 +5,14 @@ import React from 'react';
 
 const ReadyToTrip = () => {
     return (
-        <div>
-            <Image className='w-full object-cover relative overflow-hidden' src={'/assets/CTA.png'} width={1200} height={150} alt=''></Image>
-            <div className=''>
-                <h1>Ready to Start Your Journey?</h1>
-                <p>Join thousands of travelers who have discovered the world with us</p>
-                <Button>BOOK YOUR TRIP TODAY <ArrowRight></ArrowRight></Button>
+        <div className=' relative'>
+            
+
+            <Image className='w-full  object-cover relative overflow-hidden' src={'/assets/CTA.png'} width={1200} height={150} alt=''></Image>
+            <div className=' absolute top-1/2 left-1/2  -translate-1/2 text-center text-white space-y-1'>
+                <h1 className='text-5xl font-bold leading-tight'>Ready to Start Your Journey?</h1>
+                <p className='text-lg'>Join thousands of travelers who have discovered the world with us</p>
+                <Button className={'rounded-md bg-white '} variant='outline'>BOOK YOUR TRIP TODAY <ArrowRight></ArrowRight></Button>
             </div>
         </div>
     );
