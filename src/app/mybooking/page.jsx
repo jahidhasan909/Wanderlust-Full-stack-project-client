de7a84eb7047081ba3ac22f41a9ea1452e890059
jahidhasan909 <jjjjahidhasan999@gmail.com>
2026-05-16 03:04:50 +0600

    booking page add jwt verify system

--- src/app/mybooking/page.jsx
+++ src/app/mybooking/page.jsx
@@ -5,32 +5,40 @@ import React from 'react';
 
 const MyBookingPage = async () => {
 
-    const session = await auth.api.getSession({
-        headers: await headers()
-    })
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
-    const user = session?.user
+  const user = session?.user
 
-    const res = await fetch(`http://localhost:5001/booking/${user?.id}`)
-    const userBookingData = await res.json()
+  const { token } = await auth.api.getToken({
+    headers: await headers()
+  })
 
-    
+  const res = await fetch(`http://localhost:5001/booking/${user?.id}`,{
+    headers:{
+      authorization:`Bearar ${token}`
+    }
+  })
+  const userBookingData = await res.json()
 
 
-    return (
-        <div className='container mx-auto mt-35 mb-20 '>
-          <div className='px-3'>
-            <h1 className='font-bold text-3xl text-white'>My Bookings</h1>
-          <p className='text-neutral-400'>Manage and view your upcoming travel plans</p>
 
-          </div>
-          <div className=' space-y-4 mt-8'>
-            {
-                userBookingData.map(bookingData=><UserBookingDataCard key={bookingData._id} bookingData={bookingData}></UserBookingDataCard>)
-            }
-          </div>
-        </div>
-    );
+
+  return (
+    <div className='container mx-auto mt-35 mb-20 '>
+      <div className='px-3'>
+        <h1 className='font-bold text-3xl text-white'>My Bookings</h1>
+        <p className='text-neutral-400'>Manage and view your upcoming travel plans</p>
+
+      </div>
+      <div className=' space-y-4 mt-8'>
+        {
+          userBookingData.map(bookingData => <UserBookingDataCard key={bookingData._id} bookingData={bookingData}></UserBookingDataCard>)
+        }
+      </div>
+    </div>
+  );
 };
 
 export default MyBookingPage;
